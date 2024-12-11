3c936713a6dc5fea376c685ce27527e86f6ba161
BelhardTraining <an.maevskaya@gmail.com>
2024-12-11 21:20:39 +0300

    Created urgent test

--- MyTestProject/src/Main.java
+++ MyTestProject/src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void urgentTest() {
+        System.out.println("I am an urgent test");
+        System.out.println("This test is finished");
+    }
 }
