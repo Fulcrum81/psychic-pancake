7d6f7f481f30c697f69b7f0621430e25f72d32ab
BelhardTraining <an.maevskaya@gmail.com>
2024-12-11 21:43:21 +0300

    Fixed login test after conflict resolve

--- MyTestProject/src/Main.java
+++ MyTestProject/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public void myFirstTest() {
         System.out.println("I am a Login Test");
         System.out.println("I will test login functionality with correct credentials");
+    }
 
     public void urgentTest() {
         System.out.println("I am an urgent test");
